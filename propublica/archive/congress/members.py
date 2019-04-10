f4168486cf0ac6be4d28f44b1bd8f3de43d1a8db
Zachary Tyler <ztyler@pulte.com>
2019-04-10 15:33:07 -0400

    archive code in propublica and twitter modules

--- propublica/archive/congress/members.py
+++ propublica/archive/congress/members.py
@@ -1,16 +1,9 @@
 import enum
 import requests
-from five.tools.api import API, send_request
+from ...tools.api import API, send_request
 
 
-MIN_SENATE = 80
-MAX_SENATE = 115
-MIN_HOUSE = 102
-MAX_HOUSE = 115
-MIN_CONGRESS = max(MIN_HOUSE, MIN_SENATE)
-MAX_CONGRESS = min(MAX_HOUSE, MAX_SENATE)
-CONGRESS_API_ENDPOINT = 'https://api.propublica.org/congress/v1/{0}/{1}.json'
-CONGRESS_MEMBERS_URL = 'https://congress.gov/members'
+
 
 
 class CongressMembers(API):
