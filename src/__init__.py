76db9de4f1eff5801fc91ea171678969494083b7
z3c0 <z3c0@21337.tech>
2020-08-05 02:08:44 +0000

    Make get_bioguide_ids accessible

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
 """Backbone for V module"""
-from quinque.src.duo import Congress, CongressMember, search_congress_member
+from quinque.src.duo import Congress, CongressMember, search_congress_members, get_bioguide_ids
