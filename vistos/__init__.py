d6f343600f5e6c6890e04ec770ee0af2f8ea8a81
z3c0 <z3c0@21337.tech>
2020-08-27 16:08:52 +0000

    increment to v0.9.20

--- vistos/__init__.py
+++ vistos/__init__.py
@@ -5,4 +5,4 @@ from vistos.src import Congress, CongressMember, search_congress_members, gpo
 
 __all__ = ['gpo', 'Congress', 'CongressMember', 'search_congress_members']
 
-VERSION = '0.9.19'
+VERSION = '0.9.20'
