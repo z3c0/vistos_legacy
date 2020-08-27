9995167f4615c83319b116bc8d948dfd63efc88c
z3c0 <z3c0@21337.tech>
2020-08-27 18:54:20 +0000

    don't use relative path when opening version file

--- vistos/__init__.py
+++ vistos/__init__.py
@@ -1,8 +1,9 @@
 #!python3.8
 """vistos (V)"""
+import os
 
 from vistos.src import Congress, CongressMember, search_congress_members, gpo
 
 __all__ = ['gpo', 'Congress', 'CongressMember', 'search_congress_members']
 
-VERSION = str(open('./VERSION', 'r').read())
+VERSION = str(open(os.path.dirname(__file__) + '/VERSION', 'r').read())
